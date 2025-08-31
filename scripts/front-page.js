6b84566e8550c0b04ff93ecbcd34293cf4ce9c00
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2025-08-31 12:55:47 -0700

    Started login register forms and some more styling

--- scripts/front-page.js
+++ scripts/front-page.js
@@ -1,12 +1,26 @@
+// Grab the whole div with the login form.
 const loginTab = document.getElementById('login');
+
+// Grab the whole div with the register form.
 const registerTab = document.getElementById('register');
 
+// Grab the login and register tab buttons for dynamic styling.
+const loginTabBtn = document.getElementById('loginTabBtn');
+const registerTabBtn = document.getElementById('registerTabBtn');
+
+// On click of the login tab button, change to the login tab form. On click of the register tab button, change to the register tab form.
 function toggleLoginRegister(tab) {
     if(tab === loginTab) {
         loginTab.style.display = 'block';
         registerTab.style.display = 'none';
+
+        loginTabBtn.style.backgroundColor = 'rgba(7, 7, 236, 0.3)';
+        registerTabBtn.style.backgroundColor = 'black';
     } else {
         registerTab.style.display = 'block';
         loginTab.style.display = 'none';
+
+        registerTabBtn.style.backgroundColor = 'rgba(7, 7, 236, 0.3)';
+        loginTabBtn.style.backgroundColor = 'black';
     }
 };
